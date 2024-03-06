23e721e8d7465fa61e83b5296fb62792385af808
Fema123 <fema@brytonsport.com>
2024-03-06 14:48:29 +0800

    Commit from VSCode 3/6/2024

--- interview-4.py
+++ interview-4.py
@@ -5,6 +5,7 @@ arr2_set = set(arr2)
 nums1 = []
 nums2 = [] # fema test git 3/6/2024
 # fema test 2 git 3/6/2024-2
+# fema test 3 from VScode 3/6/2024
 for i in arr1:
     if i in arr2_set: nums1.append(i)
     else: nums2.append(i)
