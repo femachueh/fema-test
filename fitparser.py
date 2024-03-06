af5280a51bf7389439ed17fc7cf20f54e2143668
Fema123 <fema@brytonsport.com>
2024-03-06 19:24:51 +0800

    Add files via upload
    new file

---
+++ fitparser.py
@@ -0,0 +1,13 @@
+from fitparse import FitFile
+
+fit_file = FitFile('test.fit')
+
+with open('power_timestamp.csv', 'w') as file:
+    file.write('timestamp,power,altitude,speed\n')
+    for record in fit_file.get_messages('record'):
+        power = record.get_value('power')
+        timestamp = record.get_value('timestamp')
+        alt = record.get_value('altitude')
+        spd = record.get_value('speed')
+        if power is not None and timestamp is not None and alt is not None and spd is not None:
+            file.write(f'{timestamp},{power},{alt},{(spd*3600)/1000}\n')
