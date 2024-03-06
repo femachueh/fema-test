c0649fe3319a9db7746c0011edd27c75347b143e
Fema123 <fema@brytonsport.com>
2024-03-06 11:35:49 +0800

    fema ttt git ttt 3/6/2024

--- interview-4.py
+++ interview-4.py
@@ -3,7 +3,7 @@ arr1=[1,2,3,5,1,2,8,4,10,12]
 arr2=[1,3,4]        
 arr2_set = set(arr2)
 nums1 = []
-nums2 = []
+nums2 = [] # fema test git 3/6/2024
 for i in arr1:
     if i in arr2_set: nums1.append(i)
     else: nums2.append(i)
